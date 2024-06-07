c7da4fa2d972b56ccb5538dfec6633b2666780c2
HaudinFlorence <haudin.florence@gmail.com>
2024-06-07 13:52:25 +0200

    Update popup for the large portrait cards and fix some styling issues.

--- src/components/home/Hero/index.tsx
+++ src/components/home/Hero/index.tsx
@@ -4,32 +4,33 @@ import Astronaut from "@site/static/img/quantstack/astronaut.svg";
 
 export default function Hero() {
   return (
-    <div className="main-container-with-margins">
-      <div className={"container" + " " + styles.hero_desktop}>
-        <div className={"row row--no-gutters" + " " + styles.row_custom}>
-          <div className={"col col--6" + " " + styles.col_custom}>
-            <div className="header-text">
-              <h1 className={styles.h1_custom}>
-                Open-source for discovery, science, and education
-              </h1>
-              <p className={styles.p_custom}>
-                A team behind major open-source projects of the scientific
-                computing ecosystem
-              </p>
-              <h2 className={styles.h2_custom}>
-                Jupyter, Conda-forge, Mamba, Voilà, Xtensor and more.
-              </h2>
+    <div>
+      <div className="main-container-with-margins">
+        <div className={"container" + " " + styles.hero_desktop}>
+          <div className={"row row--no-gutters" + " " + styles.row_custom}>
+            <div className={"col col--6" + " " + styles.col_custom}>
+              <div className="header-text">
+                <h1 className={styles.h1_custom}>
+                  Open-source for discovery, science, and education
+                </h1>
+                <p className={styles.p_custom}>
+                  A team behind major open-source projects of the scientific
+                  computing ecosystem
+                </p>
+                <h2 className={styles.h2_custom}>
+                  Jupyter, Conda-forge, Mamba, Voilà, Xtensor and more.
+                </h2>
+              </div>
             </div>
-          </div>
-          <div className={"col col--6" + " " + styles.astronaut_column}>
-            <Astronaut className={styles.astronaut_picture} />
+            <div className={"col col--6" + " " + styles.astronaut_column}>
+              <Astronaut className={styles.astronaut_picture} />
+            </div>
           </div>
         </div>
       </div>
-      <div>
-        <div className={styles.logos_carousel}>
-          <SimpleSlider />
-        </div>
+
+      <div className={styles.logos_carousel}>
+        <SimpleSlider />
       </div>
     </div>
   );
