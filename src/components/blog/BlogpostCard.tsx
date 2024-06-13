f1ecc98fcd9c638f9f4514882bb26531c1a9c255
HaudinFlorence <haudin.florence@gmail.com>
2024-06-13 15:57:08 +0200

    Use the script yml-to-json.ts in /src/components/blog to produce a .json from the _config.yml including necessary modifications on the blog posts informations in particular on image widths.

--- src/components/blog/BlogpostCard.tsx
+++ src/components/blog/BlogpostCard.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import styles from "./styles.module.css";
 import Link from "@docusaurus/Link";
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export function BlogpostPicture({ blogpost }) {
   return (
@@ -12,20 +13,24 @@ export function BlogpostPicture({ blogpost }) {
   );
 }
 
-export default function BlogpostCard({ blogpost }) {
+export default function BlogpostCard({ blogpost, timeIndex }) {
+  if (blogpost.image.includes("https://")){
   return (
     <div
       className={"card " + styles.blogpost_card}
       style={{ marginBottom: "var(--ifm-spacing-lg)" }}
     >
+      <div className={styles.blog_time_index}>{timeIndex}</div>
       <Link href={blogpost.url}>
         <div className="container">
           <div className={"row" + " " + styles.row_custom}></div>
+
           <div className={styles.blogpost_image_container}>
             <div className={styles.blogpost_image}>
-              <img src={blogpost.image} width={blogpost.imageWidth} />
+              <img src={blogpost.image} id={blogpost.imageID} width={blogpost.imageWidth} />
             </div>
           </div>
+
           <div className={styles.blogpost_header}>{blogpost.title}</div>
 
           <div className={styles.blogpost_summary}>
@@ -47,4 +52,44 @@ export default function BlogpostCard({ blogpost }) {
       </Link>
     </div>
   );
+} else if(blogpost.image.includes('/img/'))
+{
+  return (
+    <div
+      className={"card " + styles.blogpost_card}
+      style={{ marginBottom: "var(--ifm-spacing-lg)" }}
+    >
+      <div className={styles.blog_time_index}>{timeIndex}</div>
+      <Link href={blogpost.url}>
+        <div className="container">
+          <div className={"row" + " " + styles.row_custom}></div>
+
+          <div className={styles.blogpost_image_container}>
+            <div className={styles.blogpost_image}>
+              <img src={useBaseUrl(blogpost.image)} width={blogpost.imageWidth} id={blogpost.imageID} />
+            </div>
+          </div>
+
+          <div className={styles.blogpost_header}>{blogpost.title}</div>
+
+          <div className={styles.blogpost_summary}>
+            {blogpost.summary.length < 200
+              ? blogpost.summary
+              : blogpost.summary.substring(0, 200) + "..."}
+          </div>
+
+          <div
+            className="row"
+            style={{ marginBottom: "var(--ifm-spacing-md)" }}
+          >
+            <div className="col" style={{ padding: "0px" }}>
+              <div className={styles.blogpost_date}>{blogpost.date}</div>
+              <div className={styles.blogpost_authors}>{blogpost.authors}</div>
+            </div>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+}
 }
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,13 +1,14 @@
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import BlogsComponent from "../components/blog";
+import blogpostDetails from "@site/src/components/blog/blogpostsDetails.json";
 
 
 export default function BlogPage(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout>
-      <BlogsComponent />
+      <BlogsComponent blogpostsDetails={blogpostDetails} />
     </Layout>
   );
 }
