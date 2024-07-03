774b615d034fdac9c3608b7e5746467e3e76c009
HaudinFlorence <haudin.florence@gmail.com>
2024-07-03 15:15:12 +0200

    Remove .docusaurus from git tracked directories.

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/fhaudin/dev/new.quantstack.github.io/node_modules/infima/dist/css/default/default.css"),
-  require("/home/fhaudin/dev/new.quantstack.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/fhaudin/dev/new.quantstack.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/fhaudin/dev/new.quantstack.github.io/src/css/custom.css"),
-];
