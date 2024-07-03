4c2453453dfda2a7d3b8a3b05ab279530cff8ea1
HaudinFlorence <haudin.florence@gmail.com>
2024-07-03 22:56:51 +0200

    Try another rendering for the project page.

--- src/components/projects/desktop/ProjectDesktop.tsx
+++ src/components/projects/desktop/ProjectDesktop.tsx
@@ -7,10 +7,10 @@ export default function ProjectDesktop({
   if (project.reverse === "false") {
     return (
       <div className={"container" + " " + styles.project_container}>
-        <div className={"row" + " " + "padding-none" + " " + "margin-none"}>
+        <div className={"row" + " " + "padding-none" + " " + "margin-none"+" "+ styles.row_project}>
           <div
             className={
-              "col col--6 col--offset-1" + " " + styles.col_project_text
+              "col col--6" + " " + styles.col_project_text
             }
           >
             <div className={styles.project_title}>{project.title}</div>
@@ -20,7 +20,7 @@ export default function ProjectDesktop({
           </div>
           <div
             className={
-              "col col--4" +
+              "col col--5" +
               " " +
               "flex-full-centered" +
               " " +
@@ -28,7 +28,7 @@ export default function ProjectDesktop({
               " " +
               styles.col_project_picture_right
             }
-            style={{ border: project.pictureBorderDesktop }}
+            /*style={{ border: project.pictureBorderDesktop }}*/
           >
             <div className={styles.project_picture}></div>
             <img
@@ -43,16 +43,17 @@ export default function ProjectDesktop({
   } else if (project.reverse === "true") {
     return (
       <div className={"container" + " " + styles.project_container}>
-        <div className={"row" + " " + "padding-none" + " " + "margin-none"}>
+        <div className={"row" + " " + "padding-none" + " " + "margin-none" + " "+ styles.row_project}>
           <div
             className={
-              "col col--4 col--offset-1" +
+              "col col--5" +
               " " +
              "flex-full-centered" + " "+ "padding-none"+
               " " +
               styles.col_project_picture_left
             }
-            style={{ border: project.pictureBorderDesktop }}
+            /*style={{ border: project.pictureBorderDesktop }}*/
+            style={{border: "Opx"}}
           >
             <div className={styles.project_picture}>
               <img
