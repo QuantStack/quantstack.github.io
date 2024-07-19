c982f1edc0034466d699f40795975d3cefb0c19f
HaudinFlorence <haudin.florence@gmail.com>
2024-07-19 14:08:56 +0200

    Address style issue comments on the PR.

--- src/components/projects/Header.tsx
+++ src/components/projects/Header.tsx
@@ -3,14 +3,12 @@ import HeaderMD from "./descriptions/Header.md";
 
 export default function Header() {
   return (
-    <div className={styles.header_container}>
-      <div className="container">
-        <div className="row">
-          <div className="col padding-none">
-            <h1 className={styles.header_title}>Working in the open</h1>
-            <div className={styles.header_text}>
-              <HeaderMD />
-            </div>
+    <div className={"container" + " " + styles.header_container}>
+      <div className="row">
+        <div className="col padding-none">
+          <h1 className={styles.header_title}>Working in the open</h1>
+          <div className={styles.header_text}>
+            <HeaderMD />
           </div>
         </div>
       </div>
--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -6,7 +6,7 @@ import AllProjects from "./AllProjects";
 export default function Projects() {
   return (
     <div>
-      <div>
+      <div className="horizontally-centered">
         <Header />
         <AllProjects />
       </div>
