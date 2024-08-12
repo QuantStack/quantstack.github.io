f61878edb58837db631a3f3b239f98290db6bab2
HaudinFlorence <haudin.florence@gmail.com>
2024-08-12 14:29:52 +0200

    Gather all the team informations into one single team.ts file, introduce some on hover style on the small portrait card and update some other style issues, remove condition fixing the width of the row starting from a given screen width.

--- src/components/projects/ScheduleAMeeting.tsx
+++ src/components/projects/ScheduleAMeeting.tsx
@@ -3,14 +3,10 @@ import LinkToContact from "../home/LinkToContact";
 
 export default function ScheduleAMeeting() {
   return (
-    <div className="container blue-banner-container">
-      <div className="row">
-        <div className="col">
-          Schedule a meeting and benefit from our experience in Python, C++,
-          in-browser data visualization, and high-performance computing.
-          <LinkToContact label={"CONTACT US!"} />
-        </div>
-      </div>
+    <div className="blue-banner-container">
+      Schedule a meeting and benefit from our experience in Python, C++,
+      in-browser data visualization, and high-performance computing.
+      <LinkToContact label={"CONTACT US!"} />
     </div>
   );
 }
--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -5,17 +5,13 @@ import AllProjects from "./AllProjects";
 
 export default function Projects() {
   return (
-    <div>
+    <>
       <div className="horizontally-centered">
         <Header />
         <AllProjects />
       </div>
 
-      <div>
-        <div className={styles.schedule_container}>
-          <ScheduleAMeeting />
-        </div>
-      </div>
-    </div>
+      <ScheduleAMeeting />
+    </>
   );
 }
