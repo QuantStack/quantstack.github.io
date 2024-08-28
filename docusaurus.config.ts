97a3ec28d98547dc5bd5fec4d19ca6212b9cdc97
HaudinFlorence <haudin.florence@gmail.com>
2024-08-28 16:13:48 +0200

    Try to fix the popup background and its content opened state. Update static.yml and docusaurus.config.ts files.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,11 +12,11 @@ const config: Config = {
   url: "https://quantstack.net/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/quantstack.github.io/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "/QuantStack/", // Usually your GitHub org/user name.
+  organizationName: "/HaudinFlorence/", // Usually your GitHub org/user name.
   projectName: "quantstack.github.io", // Usually your repo name.
 
   onBrokenLinks: "throw",
@@ -64,7 +64,7 @@ const config: Config = {
       title: "",
       logo: {
         alt: "QuantStack Logo",
-        src: "img/quantstack/logo-website.svg"
+        src: "img/quantstack/logo-website.svg",
       },
 
       items: [
@@ -131,96 +131,10 @@ const config: Config = {
       ],
     },
 
-    footer: {
-      links: [
-        {
-          title: " ",
-          items: [
-            {
-              label: " ",
-              className: "astronaut-footer",
-              href: "/home",
-            },
-          ],
-        },
-        
-        {
-          title: "Follow QuantStack on",
-          items: [
-            {
-              label: "GitHub",
-              className: "github-icon",
-              href: "https://github.com/QuantStack",
-            },
-            {
-              label: "LinkedIn",
-              className: "linkedin-icon",
-              href: "https://www.linkedin.com/company/quantstack/mycompany/",
-            },
-            {
-              label: "X",
-              className: "x-icon",
-              href: "https://twitter.com/QuantStack",
-            },
-          ],
-        },
-        {
-          title: "Menu ",
-          items: [
-            {
-              label: "Home",
-              href: "/home/",
-            },
-            {
-              label: "Projects",
-              href: "/projects/",
-            },
-            {
-              label: "Services",
-              href: "/services/",
-            },
-            {
-              label: "About",
-              href: "/about/",
-            },
-            {
-              label: "Careers",
-              href: "/careers/",
-            },
-            {
-              label: "Blog",
-              href: "/blog/",
-            },
-          ],
-        },
-        
-
-        {
-          title: "QuantStack office ",
-          items: [
-            {
-              html: `
-              
-                  <div class="div .address">
-                      16, avenue Curti <br/>
-                      94100 Saint-Maur-des-Fossés <br/>
-                      France
-                  </div>
-               
-                    `,
-            },
-          ],
-        },
-      ],
-
-      copyright: copyright,
-    },
-    
-      colorMode: {
-        defaultMode: 'light',
-        disableSwitch: true,
-        respectPrefersColorScheme: false,
-      
+    colorMode: {
+      defaultMode: "light",
+      disableSwitch: true,
+      respectPrefersColorScheme: false,
     },
 
     prism: {
