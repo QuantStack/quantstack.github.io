dc532388ee6da069b74c0da1d60a856b9702f38a
HaudinFlorence <haudin.florence@gmail.com>
2024-10-31 00:00:03 +0100

    Update SmallPortraitCardWithDialog to enable the possibility to close the dialog by clicking the overlay.

--- src/components/about/SmallPortraitCard.tsx
+++ src/components/about/SmallPortraitCard.tsx
@@ -2,9 +2,9 @@ import styles from "./styles.module.css";
 import { useHistory } from "react-router";
 import Avatar from "./Avatar";
 import { useState } from "react";
+import Link from "@docusaurus/Link";
 
-
-export function SmallPortraitCard ({ person}) {
+export function SmallPortraitCard({ person }) {
   let [isDialogOpen, setIsDialogOpen] = useState(false);
 
   const history = useHistory();
@@ -12,31 +12,29 @@ export function SmallPortraitCard ({ person}) {
   const openDialog = () => {
     history.push("/about/" + person.firstName);
     setIsDialogOpen(true);
-    /* dialog = name
-    /* use a props*/
   };
 
   return (
     <div onClick={openDialog}>
-       <div className={"card" + " " + styles.small_portrait_card}>
-      <div className="card__header">
-        <Avatar person={person} />
-        <div
-          className={
-            "flex-full-centered" + " " + styles.small_card_complete_name
-          }
-        >
-          {person.completeName}
+      <div className={"card" + " " + styles.small_portrait_card}>
+        <div className="card__header">
+          <Avatar person={person} />
+          <div
+            className={
+              "flex-full-centered" + " " + styles.small_card_complete_name
+            }
+          >
+            {person.completeName}
+          </div>
+        </div>
+        <div className="card__body">
+          <div
+            className={"flex-full-centered" + " " + styles.small_card_position}
+          >
+            {person.position}
+          </div>
         </div>
       </div>
-      <div className="card__body">
-        <div
-          className={"flex-full-centered" + " " + styles.small_card_position}
-        >
-          {person.position}
-        </div>
-      </div>
-    </div>
     </div>
   );
 }
