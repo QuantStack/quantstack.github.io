31be74ec3428a05864c77c6e99c6f3bd05fe32fd
HaudinFlorence <haudin.florence@gmail.com>
2025-01-23 10:23:11 +0100

    Update QuantStack social media icons and links.

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,6 @@ import Link from "@docusaurus/Link";
 import styles from "./styles.module.css";
 import GHPicture from "@site/static/img/socialmedias/GH.svg";
 import LinkedInPicture from "@site/static/img/socialmedias/LinkedIn.svg";
-import XPicture from "@site/static/img/socialmedias/X.svg";
 import BlueskyPicture from "@site/static/img/socialmedias/Bluesky.svg";
 import MastodonPicture from "@site/static/img/socialmedias/Mastodon.svg"
 
@@ -15,7 +14,6 @@ export function SocialMediasQuantStack() {
         <Link href={"https://www.linkedin.com/company/quantstack/mycompany"}>
           {<LinkedInPicture />}
         </Link>
-        <Link href={"https://twitter.com/QuantStack"}>{<XPicture />}</Link>
         <Link href={"https://bsky.app/profile/quantstack.bsky.social"}>{<BlueskyPicture />}</Link>
         <Link href={"https://mastodon.social/@QuantStack "}>{<MastodonPicture />}</Link>
       </div>
