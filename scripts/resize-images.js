e0cec895cdc5781043835be78f1e00bf4818ddab
Florence Haudin <haudin.florence@gmail.com>
2025-06-23 13:00:55 +0200

    Update resize-image.js script.

--- scripts/resize-images.js
+++ scripts/resize-images.js
@@ -2,48 +2,43 @@ const sharp = require('sharp');
 const fs = require('fs');
 const path = require('path');
 const sizeOf = require('image-size');
-const inputDir = path.join(__dirname, '../static', 'img', 'blogposts', 'full-size-images');
-const outputDir = path.join(__dirname, '../static', 'img', 'blogposts', 'resized-images');
+const fullSizeDir = path.join(__dirname, '../static', 'img', 'blogposts', 'full-size-images');
+const reducedSizeDir = path.join(__dirname, '../static', 'img', 'blogposts', 'resized-images');
 const containerHeight = 180;
 const containerWidth = 273;
-const containerAspectRatio = containerWidth / containerHeight;
-console.log('container Aspect Ratio:', containerAspectRatio);
 
-if (!fs.existsSync(outputDir)) {
-    fs.mkdirSync(outputDir, { recursive: true });
+if (!fs.existsSync(reducedSizeDir)) {
+    fs.mkdirSync(reducedSizeDir, { recursive: true });
 }
 
-fs.readdirSync(inputDir).forEach((file) => {
-    const inputPath = path.join(inputDir, file);
-    if (fs.existsSync(inputPath)) {
-        const dimensions = sizeOf(inputPath);
-
-        const aspectRatio = dimensions.width / dimensions.height;
-        const outputPath = path.join(outputDir, file);
-        let targetWidth, targetHeight
-        let width;
-        if (aspectRatio <= containerAspectRatio) {
-            targetHeight = containerHeight
-            targetWidth = Math.round(targetHeight * aspectRatio);
-        } else {
-            targetWidth = containerWidth*2; /* factor 2 to increase the resolution*/
-            targetHeight = Math.round(targetWidth / aspectRatio);
-        }
-
-        // Check that the image fits within the container
-            if (targetWidth > containerWidth * 2) {
-                targetWidth = containerWidth * 2;
-                targetHeight = Math.round(targetWidth / aspectRatio);
-            }
+function calculateImageSize(fullSizePath) {
+    if (fs.existsSync(fullSizePath)) {
+        const width = sizeOf(fullSizePath).width;
+        const height = sizeOf(fullSizePath).height;
+        let targetWidth, targetHeight;
+        if (width * containerHeight > containerWidth * height) {
+            targetWidth = containerWidth * 2;
+            targetHeight = Math.round(targetWidth / width * height);
             if (targetHeight > containerHeight) {
                 targetHeight = containerHeight;
-                targetWidth = Math.round(targetHeight * aspectRatio);
+                targetWidth = Math.round(targetHeight * width / height);
             }
-
-        if (/\.(png|jpg)$/i.test(file)) {
-            sharp(inputPath)
-                .resize(targetWidth, targetHeight)
-                .toFile(outputPath)
         }
+        else {
+            targetHeight = containerHeight;
+            targetWidth = Math.round(targetHeight / width * height);
+        }
+        return [targetWidth, targetHeight];
     }
-});
+}
+
+fs.readdirSync(fullSizeDir).forEach((file) => {
+    const fullSizePath = path.join(fullSizeDir, file);
+    const reducedSizePath = path.join(reducedSizeDir, file);
+    const [targetWidth, targetHeight] = calculateImageSize(fullSizePath);
+    if (/\.(png|jpg)$/i.test(file)) {
+        sharp(fullSizePath)
+            .resize(targetWidth, targetHeight)
+            .toFile(reducedSizePath)
+    }
+})
