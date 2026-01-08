86f45bc29f1c04d82a9cb51d2499ff5915fbe7a3
Florence Haudin <haudin.florence@gmail.com>
2026-01-08 15:46:37 +0100

    Update fundableProjectsDetails: remove parquet viewer project and remove unused icons.

--- src/components/fundable/projectsDetails.ts
+++ src/components/fundable/projectsDetails.ts
@@ -3,13 +3,6 @@ import JupyterGISRasterProcessingMD from "@site/src/components/fundable/descript
 import JupyterGISToolsForPythonAPIMD from "@site/src/components/fundable/descriptions/JupyterGISToolsForPythonAPI.md"
 import EmscriptenForgePackageRequestsMD from "@site/src/components/fundable/descriptions/EmscriptenForgePackageRequests.md"
 import JupyterLabParquetFileViewerMD from "@site/src/components/fundable/descriptions/JupyterLabParquetFileViewer.md"
-import JupyterGISIcon from "/img/icons/JupyterGIS.svg";
-import ToolsIcon from "/img/icons/Tools.svg";
-import PythonIcon from "/img/icons/Python.svg";
-import GlobeIcon from "/img/icons/Globe.svg";
-import EmptyIcon from "/img/icons/Empty.svg";
-import JupyterIcon from "/img/icons/Jupyter.svg";
-import EmscriptenForgeIcon from "/img/icons/EmscriptenForge.svg";
 
 export const fundableProjectsDetails = {
     jupyterEcosystem: [
@@ -19,7 +12,6 @@ export const fundableProjectsDetails = {
             pageName: "NbconvertModernization",
             shortDescription: "Conversion of Jupyter notebooks to PDF currently relies on nbconvert in the backend, which in turns uses a headless browser for producing the PDF. We propose to directly perform the PDF conversion in the user's browser, which will simplify the architecture and make it function with JupyterLite.",
             description: NbconvertModernizationMD,
-            icons: [JupyterIcon, EmptyIcon, EmptyIcon, EmptyIcon, EmptyIcon, EmptyIcon],
             price: "15000 €",
             maxNbOfFunders: 3,
             currentNbOfFunders: 2,
@@ -33,7 +25,6 @@ export const fundableProjectsDetails = {
             pageName: "JupyterGISRasterProcessing",
             shortDescription: "JupyterGIS currently offers a set of vector processing and conversion tools. These capabilities are powered by a GDAL WebAssembly (WASM) build running in the browser. We will work on extending support to raster processing tools using the same underlying technology.",
             description: JupyterGISRasterProcessingMD,
-            icons: [JupyterIcon, JupyterGISIcon, ToolsIcon, PythonIcon, GlobeIcon, EmptyIcon],
             price: "15000 €",
             maxNbOfFunders: 4,
             currentNbOfFunders: 2,
@@ -46,26 +37,12 @@ export const fundableProjectsDetails = {
             pageName: "JupyterGISToolsForPythonAPI",
             shortDescription: "JupyterGIS currently supports several vector processing and conversion tools, currently available only through the JupyterGIS user interface. We plan to extend these capabilities to the JupyterGIS Python API, enabling users to access the same processing tools programmatically. ",
             description: JupyterGISToolsForPythonAPIMD,
-            icons: [JupyterIcon, JupyterGISIcon, ToolsIcon, PythonIcon, GlobeIcon, EmptyIcon],
             price: "10000 €",
             maxNbOfFunders: 2,
             currentNbOfFunders: 0,
             currentFundingPercentage: 15,
             repoLink: "https://github.com/geojupyter/jupytergis"
         },
-        {
-            category: "Jupyter Ecosystem",
-            title: "Parquet File Viewer For JupyterLab",
-            pageName: "JupyterLabParquetFileViewer",
-            shortDescription: "",
-            description: JupyterLabParquetFileViewerMD,
-            icons: [JupyterIcon, EmptyIcon, EmptyIcon, EmptyIcon, EmptyIcon, EmptyIcon],
-            price: "10000 €",
-            maxNbOfFunders: 3,
-            currentNbOfFunders: 1,
-            currentFundingPercentage: 15,
-            repoLink: "https://github.com/jupyterlab/jupyterlab"
-        }
     ],
     packageManagement: [
         {
@@ -74,7 +51,6 @@ export const fundableProjectsDetails = {
             pageName: "EmscriptenForgePackageRequests",
             shortDescription: "Emscripten-forge is a conda package distribution specifically designed for WebAssembly. While the number of available emscripten-forge packages is growing quickly, many packages are still missing from the ecosystem. We will be working on adding new packages upon request.",
             description: EmscriptenForgePackageRequestsMD,
-            icons: [EmscriptenForgeIcon, EmptyIcon, EmptyIcon, EmptyIcon, EmptyIcon, EmptyIcon],
             price: "TBD",
             maxNbOfFunders: 1,
             currentNbOfFunders: 0,
