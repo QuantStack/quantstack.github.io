d280ef0df81510f63633dc8be02edb76b7482008
Sylvain Corlay <sylvain.corlay@gmail.com>
2026-02-04 22:03:48 +0100

    Update layout

--- src/components/home/Hero/Banner.tsx
+++ src/components/home/Hero/Banner.tsx
@@ -1,19 +1,16 @@
 // src/components/PageBanner.tsx
 import React from 'react';
-import BannerSvg from '@site/static/img/banner/notebook-link-banner.svg';
 import styles from "./styles.module.css";
 import LinkToNotebookLink from './LinkToNotebookLink';
 
 export default function Banner() {
     return (
         <div className={styles.banner_container}>
-            <BannerSvg className={styles.banner_image} role="img" />
             <div className={styles.banner_text_overlay}>
                 <div className={styles.banner_text_overlay_title}>Introducing <span className={styles.notebook_link}>Notebook.link</span></div>
                 <div className={styles.banner_text_overlay_subtile}>The future of notebook sharing</div>
                  <LinkToNotebookLink label={"Get started"}/>
             </div>
-           
         </div>
     );
 }
