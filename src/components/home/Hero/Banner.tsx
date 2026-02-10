b107065580caffd8db57651bd37dc9c5cedeef5f
Florence Haudin <haudin.florence@gmail.com>
2026-02-10 10:27:24 +0100

    Update Notebook.link banner.

--- src/components/home/Hero/Banner.tsx
+++ src/components/home/Hero/Banner.tsx
@@ -5,11 +5,35 @@ import LinkToNotebookLink from './LinkToNotebookLink';
 
 export default function Banner() {
     return (
-        <div className={styles.banner_container}>
-            <div className={styles.banner_text_overlay}>
-                <div className={styles.banner_text_overlay_title}>Introducing <span className={styles.notebook_link}>Notebook.link</span></div>
-                <div className={styles.banner_text_overlay_subtile}>The future of notebook sharing</div>
-                 <LinkToNotebookLink label={"Get started"}/>
+        <div>
+            <div className={styles.banner_container_small}>
+                <div className={styles.banner_text_overlay}>
+                    <div className={styles.banner_text_overlay_title}>Introducing <span className={styles.notebook_link}>Notebook.link</span></div>
+                    <div className={styles.banner_text_overlay_subtile}>The future of notebook sharing</div>
+                    <LinkToNotebookLink label={"Get started"} />
+                </div>
+            </div>
+            <div className={styles.banner_container_medium}>
+                
+                <div className={styles.banner_text_overlay}>
+                    <div className={styles.banner_text_overlay_title}>Introducing <span className={styles.notebook_link}>Notebook.link</span></div>
+                    <div className={styles.banner_text_overlay_subtile}>The future of notebook sharing</div>
+                    <LinkToNotebookLink label={"Get started"} />
+                </div>
+            </div>
+            <div className={styles.banner_container_large}>
+                <div className={styles.banner_text_overlay}>
+                    <div className={styles.banner_text_overlay_title}>Introducing <span className={styles.notebook_link}>Notebook.link</span></div>
+                    <div className={styles.banner_text_overlay_subtile}>The future of notebook sharing</div>
+                    <LinkToNotebookLink label={"Get started"} />
+                </div>
+            </div>
+            <div className={styles.banner_container_very_large}>
+                <div className={styles.banner_text_overlay}>
+                    <div className={styles.banner_text_overlay_title}>Introducing <span className={styles.notebook_link}>Notebook.link</span></div>
+                    <div className={styles.banner_text_overlay_subtile}>The future of notebook sharing</div>
+                    <LinkToNotebookLink label={"Get started"} />
+                </div>
             </div>
         </div>
     );
